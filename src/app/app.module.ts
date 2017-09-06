17e7a7a36a9bb98d6934da6a16a1dbd5099bea7d
Yoon Jee Woo <yoonjeewoo@gmail.com>
2017-09-06 19:33:09 +0900

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,45 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { HomeComponent } from './home/home.component';
+import { LectureComponent } from './lecture/lecture.component';
+import { MypageComponent } from './mypage/mypage.component';
+import { NoticeComponent } from './notice/notice.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'notice', component: NoticeComponent},
+	{ path: 'lecture', component: LectureComponent},
+	{ path: 'mypage', component: MypageComponent},
+	{ path: 'content', component: ContentComponent},
+	{ path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    HomeComponent,
+		NoticeComponent,
+		MypageComponent,
+		LectureComponent,
+		SidebarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+		RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
